90779c72fb6bc38df92a21d56f594a50703aceae
thenithinbalaji <thenithinbalaji@gmail.com>
2022-11-25 22:17:46 +0530

    ran black formatting

--- update.py
+++ update.py
@@ -7,10 +7,11 @@ for obj in scandir("intros/"):
         md_file = open(f"intros/{obj.name}")
         fileObjString += f"{{name: `{obj.name}`, content:`{md_file.read()}`}},"
         md_file.close()
+
 fileObjString += "] \n\n"
 f_init = open("index_init.js")
 c = f_init.read()
 f_init.close()
-f = open("index.js", 'w')
+f = open("index.js", "w")
 f.write(fileObjString + c)
 f.close()
