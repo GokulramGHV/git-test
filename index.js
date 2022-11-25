1603c974a06b7b898d39695708feabecd362aef4
thenithinbalaji <thenithinbalaji@gmail.com>
2022-11-25 22:08:32 +0530

    removed comments from index.js

--- index.js
+++ index.js
@@ -3,22 +3,6 @@ var fileNames = [{name: `sample.md`, content:`## Hello world`},{name: `nithin.md
 ![img](https://repository-images.githubusercontent.com/373582499/876f9800-c4be-11eb-9111-ecf555718c39)
 `},{name: `sample2.md`, content:`## Code
 
-Inline `code`
-
-Indented code
-
-    // Some comments
-    line 1 of code
-    line 2 of code
-    line 3 of code
-
-
-Block code "fences"
-
-```
-Sample text here...
-```
-
 ## Images
 ![Minion](https://octodex.github.com/images/minion.png)
 
